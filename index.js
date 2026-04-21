a71f71dd1a97efbe59402934db4ebb90483fadcd
Radha Raman <132103976+RadhaRaman16@users.noreply.github.com>
2026-04-21 17:33:31 +0530

    Feat/multiply (#1)
    * add multiplication function to index.js
    
    * add function call for multiplication in index.js
    
    * add additional multiplication function call in index.js
    
    * add additional multiplication function call in index.js
    
    * add additional multiplication function call in index.js

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ function sub(a, b) {
   return a - b;
 }
 
+function mul(a, b) {
+  return a * b;
+}
+
 add(2, 3);
 add(5, 7);
 add(10, 15);
 add(20, 25);
 
 sub(40, 35);
+
+mul(4, 5);
+mul(6, 7);
+mul(8, 9);
+mul(10, 11);
